0591379f13aea9335f99966227d6039afc05bc18
김상은 <81676542+SangAu124@users.noreply.github.com>
2025-02-13 09:31:58 +0900

    Init :: project

---
+++ TaskFlowAI/TaskFlowAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskFlowAIApp.swift
+//  TaskFlowAI
+//
+//  Created by moderntec_sw on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskFlowAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaskFlowAITests/TaskFlowAITests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskFlowAITests.swift
+//  TaskFlowAITests
+//
+//  Created by moderntec_sw on 2/13/25.
+//
+
+import Testing
+@testable import TaskFlowAI
+
+struct TaskFlowAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
